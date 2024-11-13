f794839a26899036164c471e96f60dcff28491c6
ana maria <anamaria62003@gmail.com>
2024-11-13 12:44:05 -0500

    se hicieorn ejemplos de  toupper y to loower

--- fundamentosJs/recursos/js/main.js
+++ fundamentosJs/recursos/js/main.js
@@ -1,47 +1,12 @@
-document.getElementById("demo").innerHTML = "hello javascript";
-document.getElementById("demo").style.fontSize = "35px";
-
-
-/*salida */
-
-window.alert(5+2);
-console.log(6+2);
-
-
-/*declaracion*/
-
-let a,b,c;
-a= 5;
-b= 6;
-c= a + b;
-window.alert(c);
-document.getElementById("demo").innerHTML= c;
-
-
-/*variable*/
-
-let p,q,r;
-p =5;
-q=7;
-const m=8;
-r = p+q+m;
-document.getElementById("demo1").innerHTML =r;
-
-
-/*tipo de dato*/
-
-let num = 24;//entero
-let name = " ana maria";//cadena
-let km =14.5;//decimal
-let isColombiano = true;//boleano
-let obj = {firstname: "ana",lastname: "perez"};//objeto
-
-let y= 17 + " poo ";
-let unir = "17" + "poo";
-window.alert(y);
-window.alert(unir);
-
 /*funcion*/
-function myfunction(){
-    document.getElementById("demo2").innerHTML = "new paragraph";
+function convertirTextoAMinusculas() {
+    let textoUsuario = document.getElementById("entradaTexto").value;
+    let textoMinusculas = String(textoUsuario).toLowerCase();
+    document.getElementById("textoUsuario").innerHTML += " " + textoMinusculas + "<br>";
+}
+
+function convertirTextoAMayusculas() {
+    let textoUsuario = document.getElementById("entradaTexto").value;
+    let textoMayusculas = String(textoUsuario).toUpperCase();
+    document.getElementById("textoUsuario").innerHTML += " " + textoMayusculas + "<br>";
 }
